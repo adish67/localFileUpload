f1b6ae93640441b4dbe5d850fed4b9fe6cc6e296
adish67 <adishpatle1998@gmail.com>
2019-01-10 14:23:22 +0530

    remove img type

--- app.js
+++ app.js
@@ -15,26 +15,26 @@ const storage = multer.diskStorage({
 const upload = multer({
   storage: storage,
   limits:{fileSize: 1000000000},
-  fileFilter: function(req, file, cb){
-    checkFileType(file, cb);
-  }
-}).single('myImage');
+  // fileFilter: function(req, file, cb){
+  //   checkFileType(file, cb);
+  // }
+}).single('myFile');
 
-// Check File Type
-function checkFileType(file, cb){
-  // Allowed ext
-  const filetypes = /jpeg|jpg|png|gif/;
-  // Check ext
-  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-  // Check mime
-  const mimetype = filetypes.test(file.mimetype);
+// // Check File Type
+// function checkFileType(file, cb){
+//   // Allowed ext
+//   const filetypes = /jpeg|jpg|png|gif/;
+//   // Check ext
+//   const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+//   // Check mime
+//   const mimetype = filetypes.test(file.mimetype);
 
-  if(mimetype && extname){
-    return cb(null,true);
-  } else {
-    cb('Error: Images Only!');
-  }
-}
+//   if(mimetype && extname){
+//     return cb(null,true);
+//   } else {
+//     cb('Error: Images Only!');
+//   }
+// }
 
 // Init app
 const app = express();
